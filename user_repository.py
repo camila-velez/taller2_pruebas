33e4d6497b50cc1429f281bedcb96cf977937d6c
Camila Vélez <141354994+camila-velez@users.noreply.github.com>
2026-04-08 14:35:23 +0000

    taller hasta el punto 10

--- user_repository.py
+++ user_repository.py
@@ -1,5 +1,5 @@
 import requests
-
+"""
 class JsonPlaceholderUserRepository:
     def get_user_email(self, user_id):
         response = requests.get(
@@ -8,15 +8,12 @@ class JsonPlaceholderUserRepository:
         response.raise_for_status()
         return response.json()['email']
 
-"""
 class JsonPlaceholderUserRepository:
     def get_user_email(self, user_id):
         raise ConnectionError("User service unavailable")
-"""
 
 """
 class FakeUserRepository:
     def get_user_email(self, user_id):
         # TODO: retornar un email ficticio
-        _________________________________
-"""
+        return f'user{user_id}@fake.local'
